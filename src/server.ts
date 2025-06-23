ce8d1c7b0b7809e6e2e5c973263589318d950184
givebackacademie <givebackacademie@gmail.com>
2025-06-23 11:10:30 +0100

    inital1

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+// import dotenv from 'dotenv';
 import {
   AngularNodeAppEngine,
   createNodeRequestHandler,
@@ -6,12 +7,15 @@ import {
 } from '@angular/ssr/node';
 import express from 'express';
 import { join } from 'node:path';
+// import { environments } from './environments/environment';
 
 const browserDistFolder = join(import.meta.dirname, '../browser');
-
- const app = express();
+// import { initializeApp, applicationDefault, getApps } from 'firebase-admin/app';
+const app = express();
 const angularApp = new AngularNodeAppEngine();
-
+// import * as serviceAccount from '../serviceAccountKey.json';
+// Firestore instance
+require('dotenv').config();
 /**
  * Example Express Rest API endpoints can be defined here.
  * Uncomment and define endpoints as necessary.
@@ -23,6 +27,51 @@ const angularApp = new AngularNodeAppEngine();
  * });
  * ```
  */
+
+var admin = require('firebase-admin');
+if (admin.apps.length === 0) {
+  await admin.initializeApp({
+    credential: admin.credential.cert(process.env['SECRET_KEYS']),
+  });
+}
+
+app.get('/sitemap.xml', async (req, res) => {
+  const db = await admin.firestore();
+
+  const allurls = [];
+  const snapshot = await db.collection('carousel').get();
+  const posts = snapshot.docs.map((doc: any) => ({ id: doc.id, ...doc.data() }));
+  const urls = posts
+    .map((doc: any) => {
+      const slug = doc['slug'];
+      return `<url><loc>http://localhost:4200/post/${slug}
+      </loc>
+         <lastmod>2022-06-04</lastmod>
+      </url>`;
+    })
+    .join('\n');
+  allurls.push(urls);
+  const snapshot2 = await db.collection('topics').get();
+  const posts2 = snapshot2.docs.map((doc: any) => ({ id: doc.id, ...doc.data() }));
+  const urls2 = posts2
+    .map((doc: any) => {
+      const slug = doc['slug']; // or use doc.data().slug
+      console.log(slug);
+      return `<url><loc>http://localhost:4200/post/${slug}
+      </loc>
+         <lastmod>2022-06-04</lastmod>
+      </url>`;
+    })
+    .join('\n');
+  allurls.push(urls2);
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  ${allurls.join('\n')}
+  </urlset>`;
+  console.log(sitemap);
+  res.header('Content-Type', 'application/xml');
+  res.send(sitemap);
+});
 
 /**
  * Serve static files from /browser
@@ -41,9 +90,7 @@ app.use(
 app.use((req, res, next) => {
   angularApp
     .handle(req)
-    .then((response) =>
-      response ? writeResponseToNodeResponse(response, res) : next(),
-    )
+    .then((response) => (response ? writeResponseToNodeResponse(response, res) : next()))
     .catch(next);
 });
 
